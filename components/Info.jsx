01b6970d9f0f2d89bc9812c0cda5875ef77c1138
Antac69 <angello_04_09@hotmail.com>
2023-07-10 23:06:50 -0500

    agregando style a la seccion de cambio de tema

--- components/Info.jsx
+++ components/Info.jsx
@@ -4,19 +4,20 @@ import InfoTop from "./info/InfoTop";
 import InfoCards from "./info/InfoCards";
 import { useColorScheme } from '@mui/joy/styles';
 import Button from '@mui/joy/Button';
+import { Grid } from "@mui/joy";
 const Info = () => {
   const { mode, setMode } = useColorScheme();
   return (
     <section className={styles.container}>
       <InfoTop></InfoTop>
       <InfoCards></InfoCards>
-      <section className={styles.bottom}>Theme: <Button
+      <Grid className={styles.bottom} p={2} spacing='3em'>Theme: <Button
       variant="outlined"
       color="neutral"
       onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
     >
       {mode === 'dark' ? 'Turn light' : 'Turn dark'}
-    </Button></section>
+    </Button></Grid>
     </section>
   );
 };
